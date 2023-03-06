0e4024eca41b86ebe949b9858fd6d1e7ce8c1f54
ellisrichardj <richard.ellis@apha.gov.uk>
2023-03-06 17:32:26 +0000

    Comment unused imports

--- src/components/controls/controls.js
+++ src/components/controls/controls.js
@@ -2,29 +2,30 @@ import React from "react";
 import { useTranslation } from 'react-i18next';
 
 import ColorBy, {ColorByInfo} from "./color-by";
-import DateRangeInputs, {DateRangeInfo} from "./date-range-inputs";
-import AnimationControls from "./animation-controls";
-import ChooseExplodeAttr from "./choose-explode-attr";
 import ChooseBranchLabelling from "./choose-branch-labelling";
-import ChooseLayout from "./choose-layout";
 import ChooseDataset from "./choose-dataset";
-import ChooseSecondTree from "./choose-second-tree";
 import ChooseTipLabel from "./choose-tip-label";
-import ChooseMetric from "./choose-metric";
 import PanelLayout from "./panel-layout";
 import GeoResolution from "./geo-resolution";
-import TransmissionLines from './transmission-lines';
-import NormalizeFrequencies from "./frequency-normalization";
-import AnimationOptions from "./animation-options";
 import PanelToggles from "./panel-toggles";
-import ToggleTangle from "./toggle-tangle";
 import Language from "./language";
 import { ControlsContainer } from "./styles";
 import FilterData, {FilterInfo} from "./filter";
-import {TreeOptionsInfo, MapOptionsInfo, AnimationOptionsInfo, PanelOptionsInfo,
-  ExplodeTreeInfo, FrequencyInfo, MeasurementsOptionsInfo} from "./miscInfoText";
 import { AnnotatedHeader } from "./annotatedHeader";
-import MeasurementsOptions from "./measurementsOptions";
+import {TreeOptionsInfo, MapOptionsInfo, PanelOptionsInfo, FrequencyInfo/* AnimationOptionsInfo, 
+ExplodeTreeInfo,, MeasurementsOptionsInfo*/} from "./miscInfoText";
+
+//import DateRangeInputs, {DateRangeInfo} from "./date-range-inputs";
+//import AnimationControls from "./animation-controls";
+//import ChooseExplodeAttr from "./choose-explode-attr";
+//import ChooseLayout from "./choose-layout";
+//import ChooseSecondTree from "./choose-second-tree";
+//import ChooseMetric from "./choose-metric";
+//import TransmissionLines from './transmission-lines';
+//import NormalizeFrequencies from "./frequency-normalization";
+//import AnimationOptions from "./animation-options";
+//import ToggleTangle from "./toggle-tangle";
+//import MeasurementsOptions from "./measurementsOptions";
 
 function Controls({mapOn, frequenciesOn, measurementsOn, mobileDisplay}) {
   const { t } = useTranslation();
